278f064161a10b29fd4bcbbe9860700edc77f56c
신철헌 <choelheon_shin@tmax.co.kr>
2022-11-16 09:35:29 +0900

    [feat] Button 컴포넌트 추가
    Button 컴포넌트 추가

---
+++ src/components/ButtonComponent.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './button.css';
+
+export interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: 'small' | 'medium' | 'large';
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button: React.FC<ButtonProps> = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  ...props
+}) => {
+  return (
+    <button>
+      {label}
+    </button>
+  );
+};

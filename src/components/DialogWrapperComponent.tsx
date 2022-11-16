9360e301a718204bbc303f845bccf0cee1cd3c31
kunwoo_kim5 <kunwoo_kim5@tmax.co.kr>
2022-11-16 10:35:08 +0900

    [feat] DialogWrapperComponent 추가
    - DialogWrapperComponent 추가

---
+++ src/components/DialogWrapperComponent.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { observer } from 'mobx-react-lite';
+import useStore from 'hooks/util/useStore';
+
+import 'style/dialog/Dialog.scss';
+import { DialogId, DialogInfo } from 'store/ui/dialog/DialogTypes';
+
+const DialogWrapperComponent = (): JSX.Element => {
+ 
+
+  
+
+  return <></>;
+};
+
+export default observer(DialogWrapperComponent);
